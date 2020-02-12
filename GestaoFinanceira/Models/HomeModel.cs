2cdbbb1e17612fc9f096dfdacbf85899a40118e0
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-02-12 16:25:06 -0300

    Iniciando a implementação do Projeto Teste Gestão Financeira.

---
+++ GestaoFinanceira/Models/HomeModel.cs
@@ -0,0 +1,26 @@
+﻿using GestaoFinanceira.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoFinanceira.Models
+{
+    public class HomeModel
+    {
+        public string LerNomeUsuarioModel()
+        {
+            DAL objDALModel = new DAL();
+            DataTable dtModel =  objDALModel.RetDataTable("select * from usuario");
+            if (dtModel != null)
+            {
+                if (dtModel.Rows.Count > 0)
+                {
+                    return dtModel.Rows[1]["Nome"].ToString();
+                }
+            }
+            return "Nome não encontrado";
+        }
+    }
+}
